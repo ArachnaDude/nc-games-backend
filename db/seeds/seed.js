ba607cb34e4059b104b54f6cf0eda3ca70a86372
Matt <cunning.distraction@googlemail.com>
2022-05-11 18:21:24 +0100

    create tables correctly

--- db/seeds/seed.js
+++ db/seeds/seed.js
@@ -1,10 +1,43 @@
 const db = require("../connection");
 
-const seed = (data) => {
+const seed = async (data) => {
   const { categoryData, commentData, reviewData, userData } = data;
-  // 1. create tables
+  // 1. drop tables if exist - delete in correct order
+  await db.query(`DROP TABLE IF EXISTS comments;`);
+  await db.query(`DROP TABLE IF EXISTS reviews;`);
+  await db.query(`DROP TABLE IF EXISTS users;`);
+  await db.query(`DROP TABLE IF EXISTS categories`);
 
-  // 2. insert data
+  // 2. create tables in correct order - oppsite order to drop order
+  await db.query(`CREATE TABLE categories (
+    slug TEXT PRIMARY KEY,
+    description TEXT DEFAULT 'no description available'
+  );`);
+  await db.query(`CREATE TABLE users (
+    username TEXT PRIMARY KEY,
+    avatar_url TEXT,
+    name TEXT NOT NULL
+  );`);
+  await db.query(`CREATE TABLE reviews (
+    review_id SERIAL PRIMARY KEY,
+    title TEXT NOT NULL,
+    review_body TEXT NOT NULL,
+    designer TEXT,
+    review_img_url TEXT DEFAULT 'https://images.pexels.com/photos/163064/play-stone-network-networked-interactive-163064.jpeg',
+    votes INT DEFAULT 0,
+    category TEXT NOT NULL REFERENCES categories (slug),
+    owner TEXT NOT NULL REFERENCES users (username),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+  );`);
+  await db.query(`CREATE TABLE comments (
+    comment_id SERIAL PRIMARY KEY,
+    author TEXT NOT NULL REFERENCES users (username),
+    review_id INT NOT NULL REFERENCES reviews (review_id),
+    votes INT DEFAULT 0,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    body TEXT NOT NULL
+  );`);
+  // 3. insert data
 };
 
 module.exports = seed;
