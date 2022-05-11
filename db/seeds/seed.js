f948f896227c9304c1953a0772728120aa7edd63
Matt <cunning.distraction@googlemail.com>
2022-05-11 17:15:51 +0100

    begin seed formatting

--- db/seeds/seed.js
+++ db/seeds/seed.js
@@ -1,6 +1,9 @@
+const db = require("../connection");
+
 const seed = (data) => {
   const { categoryData, commentData, reviewData, userData } = data;
   // 1. create tables
+
   // 2. insert data
 };
 
