615e5a2aefabfd028f2967903439966e87a3a259
Matt <cunning.distraction@googlemail.com>
2022-05-30 23:06:42 +0100

    function formats user data

--- db/utils/formatUserData.js
+++ db/utils/formatUserData.js
@@ -1,1 +1,7 @@
 // need to return [username, avatar_url, name]
+
+exports.formatUserData = (userData) => {
+  return userData.map((user) => {
+    return [user.username, user.avatar_url, user.name];
+  });
+};
