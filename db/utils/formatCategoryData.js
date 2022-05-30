167bc83c00be196b7df8334ab6833701830f2dd5
Matt <cunning.distraction@googlemail.com>
2022-05-30 23:06:19 +0100

    function formats category data

--- db/utils/formatCategoryData.js
+++ db/utils/formatCategoryData.js
@@ -1,7 +1,7 @@
 // need to return [slug, description]
 
 exports.formatCategoryData = (categoryData) => {
-  categoryData.map((category) => {
+  return categoryData.map((category) => {
     return [category.slug, category.description];
   });
 };
