153d566888f060a2ca2b0fd35ac784d4eb829ef4
Matt <cunning.distraction@googlemail.com>
2022-05-30 23:07:26 +0100

    function formats comment data

--- db/utils/formatCommentData.js
+++ db/utils/formatCommentData.js
@@ -4,9 +4,21 @@
   comment_id (psql),
   author,
   review_id,
-  votes,
+  votes (psql),
   created_at,
   body
 ]
 
 */
+
+exports.formatCommentData = (commentData) => {
+  return commentData.map((comment) => {
+    return [
+      comment.author,
+      comment.review_id,
+      comment.votes,
+      comment.created_at,
+      comment.body,
+    ];
+  });
+};
