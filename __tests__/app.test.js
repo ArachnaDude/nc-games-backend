ef253e5e0769d47778f70ad4ae02db62516bf624
Matt <cunning.distraction@googlemail.com>
2022-06-01 17:17:11 +0100

    begin test suite for first get function

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,6 +1,32 @@
-const db = require('../db/connection.js');
-const testData = require('../db/data/test-data/index.js');
-const seed = require('../db/seeds/seed.js');
+const db = require("../db/connection.js");
+const testData = require("../db/data/test-data/index.js");
+const seed = require("../db/seeds/seed.js");
 
+//supertest should always be brought in as "request"
+const request = require("supertest");
+
+//app should be brought in too, in order to be tested
+const app = require("../app.js");
+
+// seeds the database with test data before each test
+// to make sure tests don't interfere with each other
 beforeEach(() => seed(testData));
 afterAll(() => db.end());
+
+describe("GET /api/categories", () => {
+  test("status: 200, responds with an array of categories", () => {
+    return request(app)
+      .get("/api/categories")
+      .expect(200)
+      .then((result) => {
+        expect(result.body.categories).toBeInstanceOf(Array);
+        expect(result.body.categories).toHaveLength(4);
+        result.body.categories.forEach((category) => {
+          expect(category).toMatchObject({
+            slug: expect.any(String),
+            description: expect.any(String),
+          });
+        });
+      });
+  });
+});
