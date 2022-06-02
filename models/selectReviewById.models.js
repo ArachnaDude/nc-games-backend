9b1779e246a84aa3184907b547dae3623700e9be
Matt <cunning.distraction@googlemail.com>
2022-06-02 02:25:42 +0100

    remove pg-format from query

--- models/selectReviewById.models.js
+++ models/selectReviewById.models.js
@@ -1,21 +1,22 @@
 const db = require("../db/connection");
-const format = require("pg-format");
 
-// Format allows sanitising input, so we can't SQL inject.
-// Alternatively, we could have used comments.review_id=$1
-// which would have had the same effect without using pg-format.
+// $1 sanitises the input, guarding against SQL injection.
+// Alternatively pg-format's %s would accomplish the same thing.
+// However the error code it produces is 42703 (undefined column),
+// rather than 22P02 (invalid input).
 
 // All values sanitised in this way need to be in an array.
 
 exports.selectReviewById = (review_id) => {
-  const sql = format(
-    `SELECT reviews.*, COUNT (comments.comment_id) AS "comment_count" 
+  return db
+    .query(
+      `SELECT reviews.*, COUNT (comments.comment_id) AS "comment_count" 
 FROM reviews LEFT JOIN comments ON reviews.review_id = comments.review_id
-WHERE comments.review_id=%s GROUP BY reviews.review_id;`,
-    [review_id]
-  );
-  return db.query(sql).then((result) => {
-    console.log(result);
-    return result.rows[0];
-  });
+WHERE comments.review_id=$1 GROUP BY reviews.review_id;`,
+      [review_id]
+    )
+    .then((result) => {
+      console.log(result);
+      return result.rows[0];
+    });
 };
