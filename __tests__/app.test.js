6e4f6119a9468bb9756b6b790b416e63ffdcbf0a
Matt <cunning.distraction@googlemail.com>
2022-06-02 02:03:34 +0100

    test suite tests happy path for getReviewById

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -30,3 +30,33 @@ describe("GET /api/categories", () => {
       });
   });
 });
+describe.only("GET /api/reviews/:review_id", () => {
+  test.only("status: 200, responds with an object of the specfied review", () => {
+    return request(app)
+      .get("/api/reviews/2")
+      .expect(200)
+      .then((result) => {
+        expect(result.body.review).toBeInstanceOf(Object);
+        expect(result.body.review).toMatchObject({
+          owner: expect.any(String),
+          title: expect.any(String),
+          review_id: 2,
+          review_body: expect.any(String),
+          designer: expect.any(String),
+          review_img_url: expect.any(String),
+          category: expect.any(String),
+          created_at: expect.any(String),
+          votes: expect.any(Number),
+          comment_count: "3",
+        });
+      });
+  });
+  test("status: 400, responds with bad request when passed a badly formed request", () => {
+    return request(app)
+      .get("/api/reviews/one")
+      .expect(400)
+      .then((result) => {
+        expect(result.body.message).toBe("Bad request");
+      });
+  });
+});
