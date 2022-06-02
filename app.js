97543b5b548a2f2297b31949f139ae252a7c6a1e
Matt <cunning.distraction@googlemail.com>
2022-06-02 02:04:10 +0100

    add getReviewById middleware

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ const cors = require("cors");
 const express = require("express");
 const app = express();
 
-const { getCategories } = require("./controllers");
+// controller imports
+const { getCategories, getReviewById } = require("./controllers");
 
 // required to enable CORS for frontend use
 app.use(cors());
@@ -13,6 +14,10 @@ app.use(cors());
 // required to parse a body. req.body doesn't exist without this.
 app.use(express.json());
 
+// responds with an array of categories
 app.get("/api/categories", getCategories);
+
+// responds with a single review object
+app.get("/api/reviews/:review_id", getReviewById);
 
 module.exports = app;
