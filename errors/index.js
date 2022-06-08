268dac268a9bb469137c54163317ed47b8d52233
Matt <cunning.distraction@googlemail.com>
2022-06-08 22:44:47 +0100

    add template for status:405 errors

--- errors/index.js
+++ errors/index.js
@@ -1,6 +1,9 @@
-exports.handle404 = () => {};
+exports.handle404 = (req, res, next) => {};
+
+exports.handle405 = (req, res, next) => {};
 
 exports.handlePsqlErrors = (err, req, res, next) => {
+  console.log(err, "psql error");
   const { code } = err;
   if (code === "22P02") {
     res.status(400).send({ message: "Bad request" });
@@ -10,6 +13,7 @@ exports.handlePsqlErrors = (err, req, res, next) => {
 };
 
 exports.handleCustomErrors = (err, req, res, next) => {
+  console.log(err, "custom error");
   const { status, message } = err;
   res.status(status).send({ message });
 };
