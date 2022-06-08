59c268ce8b3af1bf8de50eabf99e867ae1153290
Matt <cunning.distraction@googlemail.com>
2022-06-08 23:43:53 +0100

    remove console.log for req.method

--- controllers/getCategories.controllers.js
+++ controllers/getCategories.controllers.js
@@ -3,7 +3,6 @@ const { selectCategories } = require("../models/selectCategories.models");
 exports.getCategories = async (req, res, next) => {
   //this is an async function - selectCategories returns a promise
   try {
-    console.log(req.method);
     const categories = await selectCategories();
     res.status(200).send({ categories });
   } catch (error) {
