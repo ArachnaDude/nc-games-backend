e0d4ed222bb10eea54a1b90679234b211234a7d8
Matt <cunning.distraction@googlemail.com>
2022-06-08 23:44:58 +0100

    add 405 error handler to route list

--- routes/reviews.router.js
+++ routes/reviews.router.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const { getReviewById } = require("../controllers");
+const { handle405 } = require("../errors");
 const reviewsRouter = express.Router();
 
+// TODO
 reviewsRouter.route("/");
 
-reviewsRouter.route("/:review_id").get(getReviewById);
+// controls all methods assigned to /api/reviews/:review_id
+reviewsRouter.route("/:review_id").get(getReviewById).all(handle405);
 
 module.exports = reviewsRouter;
