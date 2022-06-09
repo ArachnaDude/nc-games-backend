410cba49c1c4f4763db3275daec97deed4b79d50
Matt <cunning.distraction@googlemail.com>
2022-06-09 22:00:53 +0100

    index updated to add getReviews controller

--- controllers/index.js
+++ controllers/index.js
@@ -1,5 +1,6 @@
 const { getCategories } = require("./getCategories.controllers");
 const { getReviewById } = require("./getReviewById.controllers");
 const { patchReviewById } = require("./patchReviewById.controllers");
+const { getReviews } = require("./getReviews.controllers");
 
-module.exports = { getCategories, getReviewById, patchReviewById };
+module.exports = { getCategories, getReviewById, patchReviewById, getReviews };
