0c5c251099ec8e287488d653712197478d018cdc
Matt <cunning.distraction@googlemail.com>
2022-06-10 01:15:57 +0100

    add req.query to getReview controller

--- controllers/getReviews.controllers.js
+++ controllers/getReviews.controllers.js
@@ -1,10 +1,11 @@
 const { selectReviews } = require("../models/selectReviews.models");
 
 exports.getReviews = async (req, res, next) => {
-  console.log(req.query, "req.query");
+  const { category } = req.query;
+  console.log(category, "req.query");
   try {
     console.log("controller");
-    const reviews = await selectReviews();
+    const reviews = await selectReviews(category);
     res.status(200).send({ reviews });
   } catch (error) {
     next(error);
