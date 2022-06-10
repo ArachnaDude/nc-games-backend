4655ec695d5820d0c405bd2e1050c4c3f07203b9
Matt <cunning.distraction@googlemail.com>
2022-06-10 01:16:55 +0100

    refactor query string with pg-format to allow category query

--- models/selectReviews.models.js
+++ models/selectReviews.models.js
@@ -1,22 +1,24 @@
+const format = require("pg-format");
 const db = require("../db/connection");
 
-exports.selectReviews = () => {
+exports.selectReviews = (category) => {
   console.log("model");
-  return db
-    .query(
-      `SELECT reviews.owner, 
-reviews.title, 
-reviews.review_id,
-reviews.review_body,
-reviews.category,
-reviews.review_img_url,
-reviews.created_at,
-reviews.votes,
+  let queryStr = `SELECT reviews.*,
 COUNT (comments.comment_id) AS "comment_count"
-FROM reviews LEFT JOIN comments ON reviews.review_id = comments.review_id
-GROUP BY reviews.review_id ORDER BY reviews.created_at DESC;`
-    )
-    .then((result) => {
-      return result.rows;
-    });
+FROM reviews LEFT JOIN comments ON reviews.review_id = comments.review_id`;
+
+  if (category) {
+    queryStr += format(
+      `
+  WHERE reviews.category = %L`,
+      [category]
+    );
+  }
+
+  queryStr += `
+  GROUP BY reviews.review_id ORDER BY created_at DESC;`;
+  console.log(queryStr);
+  return db.query(queryStr).then((result) => {
+    return result.rows;
+  });
 };
