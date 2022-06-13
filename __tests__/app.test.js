3413ad4048a6766671f05c28f10b1cd0232caaab
Matt <cunning.distraction@googlemail.com>
2022-06-13 16:38:08 +0100

    test suite expanded for status 200 getCommentsByReviewId

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -204,3 +204,24 @@ describe("GET /api/reviews", () => {
       });
   });
 });
+describe.only("GET /api/reviews/:review_id/comments", () => {
+  test("status: 200, responds with an array of comment objects for a given review_id", () => {
+    return request(app)
+      .get("/api/reviews/3/comments")
+      .expect(200)
+      .then((result) => {
+        expect(result.body.comments).toBeInstanceOf(Array);
+        expect(result.body.comments).toHaveLength(3);
+        result.body.comments.forEach((comment) => {
+          expect(comment).toMatchObject({
+            comment_id: expect.any(Number),
+            votes: expect.any(Number),
+            created_at: expect.any(String),
+            author: expect.any(String),
+            body: expect.any(String),
+          });
+        });
+      });
+  });
+  test("status: 200, array of comment objects is sorted by 'created_at'");
+});
