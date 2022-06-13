66cd3aea2460006ff1514c9c8af75f6a9c18189d
Matt <cunning.distraction@googlemail.com>
2022-06-13 17:33:31 +0100

    remove console.log

--- controllers/getCommentsByReviewId.controllers.js
+++ controllers/getCommentsByReviewId.controllers.js
@@ -5,7 +5,6 @@ const {
 exports.getCommentsByReviewId = async (req, res, next) => {
   try {
     const { review_id } = req.params;
-    console.log("controlla");
     const comments = await selectCommentsByReviewId(review_id);
     res.status(200).send({ comments });
   } catch (error) {
