8ebbbafe8d15a87314144bf26848e9df7533affb
Matt <cunning.distraction@googlemail.com>
2022-06-13 16:40:27 +0100

    add valid method to reviews.router

--- routes/reviews.router.js
+++ routes/reviews.router.js
@@ -3,11 +3,12 @@ const {
   getReviewById,
   patchReviewById,
   getReviews,
+  getCommentsByReviewId,
 } = require("../controllers");
 const { handle405 } = require("../errors");
 const reviewsRouter = express.Router();
 
-// TODO
+// controlls all methods assigned to /api/reviews
 reviewsRouter.route("/").get(getReviews).all(handle405);
 
 // controls all methods assigned to /api/reviews/:review_id
@@ -15,6 +16,12 @@ reviewsRouter
   .route("/:review_id")
   .get(getReviewById)
   .patch(patchReviewById)
+  .all(handle405);
+
+// controlls all methods assigned to /api/reviews/:review_id/comments
+reviewsRouter
+  .route("/:review_id/comments")
+  .get(getCommentsByReviewId)
   .all(handle405);
 
 module.exports = reviewsRouter;
