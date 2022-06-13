dd721b62593159ee441f144bf57415f01cb0f103
Matt <cunning.distraction@googlemail.com>
2022-06-13 16:59:05 +0100

    model updated to allow sorting while guarding against injection

--- models/selectCommentsByReviewId.models.js
+++ models/selectCommentsByReviewId.models.js
@@ -1,10 +1,29 @@
 const db = require("../db/connection");
 
-exports.selectCommentsByReviewId = (review_id) => {
-  console.log(review_id);
+exports.selectCommentsByReviewId = (
+  review_id,
+  sort_by = "created_at",
+  order = "DESC"
+) => {
+  const validColumns = ["comment_id", "votes", "created_at", "author", "body"];
+  const validOrders = ["ASC", "DESC"];
+
+  if (
+    !validColumns.includes(sort_by) ||
+    !validOrders.includes(order.toUpperCase())
+  ) {
+    return Promise.reject({ status: 400, message: "bad request" });
+  }
+
   return db
     .query(
-      `SELECT comment_id, votes, created_at, author, body FROM comments WHERE review_id = $1;`,
+      `SELECT comment_id, 
+      votes, 
+      created_at, 
+      author, 
+      body 
+      FROM comments WHERE review_id = $1
+      ORDER BY ${sort_by} ${order};`,
       [review_id]
     )
     .then((result) => {
