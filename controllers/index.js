e1dbce26e64905702a024893e6f5387c49df76fc
Matt <cunning.distraction@googlemail.com>
2022-06-13 16:39:50 +0100

    update index with getCommentsByReviewId controller

--- controllers/index.js
+++ controllers/index.js
@@ -2,5 +2,14 @@ const { getCategories } = require("./getCategories.controllers");
 const { getReviewById } = require("./getReviewById.controllers");
 const { patchReviewById } = require("./patchReviewById.controllers");
 const { getReviews } = require("./getReviews.controllers");
+const {
+  getCommentsByReviewId,
+} = require("./getCommentsByReviewId.controllers");
 
-module.exports = { getCategories, getReviewById, patchReviewById, getReviews };
+module.exports = {
+  getCategories,
+  getReviewById,
+  patchReviewById,
+  getReviews,
+  getCommentsByReviewId,
+};
