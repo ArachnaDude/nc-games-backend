f25ee165c33ede71a6bb9a9fae790dd9a6ed285a
Matt <cunning.distraction@googlemail.com>
2022-06-14 02:38:15 +0100

    add valid route to api router

--- routes/api.router.js
+++ routes/api.router.js
@@ -3,8 +3,13 @@ const express = require("express");
 const categoriesRouter = require("./categories.router");
 const reviewsRouter = require("./reviews.router");
 const commentsRouter = require("./comments.router");
+const { handle405 } = require("../errors");
+const { getEndpoints } = require("../controllers");
 
 const apiRouter = express.Router();
+
+// this route is specifically for /api
+apiRouter.route("/").get(getEndpoints).all(handle405);
 
 // everything in here starts with /api
 // endpoints are grouped into here to keep
