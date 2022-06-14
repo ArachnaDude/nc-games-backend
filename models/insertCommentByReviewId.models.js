1019068c1a428b05af29188cd6fb4d722aea0615
Matt <cunning.distraction@googlemail.com>
2022-06-14 02:36:54 +0100

    remove console log

--- models/insertCommentByReviewId.models.js
+++ models/insertCommentByReviewId.models.js
@@ -1,8 +1,6 @@
 const db = require("../db/connection");
 
 exports.insertCommentByReviewId = (username, body, review_id) => {
-  console.log("model");
-
   return db
     .query(
       `INSERT INTO comments (author, body, review_id) VALUES ($1, $2, $3) RETURNING comment_id, votes, created_at, author, body;`,
