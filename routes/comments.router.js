68491847751682933016a818e584bb75174d8fab
Matt <cunning.distraction@googlemail.com>
2022-06-14 01:23:47 +0100

    update router with valid method

--- routes/comments.router.js
+++ routes/comments.router.js
@@ -1,6 +1,10 @@
 const express = require("express");
+const { deleteCommentById } = require("../controllers");
 
 const { handle405 } = require("../errors");
 const commentsRouter = express.Router();
+
+// controls all methods assinged to /api/comments/:comment_id
+commentsRouter.route("/:comment_id").delete(deleteCommentById).all(handle405);
 
 module.exports = commentsRouter;
