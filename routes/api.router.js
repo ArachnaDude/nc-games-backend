73379c128f156cc600dee2e8900f401eeb40732d
Matt <cunning.distraction@googlemail.com>
2022-06-14 01:06:48 +0100

    update apiRouter with comments router

--- routes/api.router.js
+++ routes/api.router.js
@@ -2,6 +2,7 @@ const express = require("express");
 
 const categoriesRouter = require("./categories.router");
 const reviewsRouter = require("./reviews.router");
+const commentsRouter = require("./comments.router");
 
 const apiRouter = express.Router();
 
@@ -12,5 +13,7 @@ const apiRouter = express.Router();
 apiRouter.use("/categories", categoriesRouter);
 
 apiRouter.use("/reviews", reviewsRouter);
+
+apiRouter.use("/comments", commentsRouter);
 
 module.exports = apiRouter;
