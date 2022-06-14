39b671584aef0cd00d898b06b9fe7e2221678b50
Matt <cunning.distraction@googlemail.com>
2022-06-14 15:52:02 +0100

    add valid route for patch request

--- routes/comments.router.js
+++ routes/comments.router.js
@@ -1,10 +1,14 @@
 const express = require("express");
-const { deleteCommentById } = require("../controllers");
+const { deleteCommentById, patchCommentById } = require("../controllers");
 
 const { handle405 } = require("../errors");
 const commentsRouter = express.Router();
 
 // controls all methods assinged to /api/comments/:comment_id
-commentsRouter.route("/:comment_id").delete(deleteCommentById).all(handle405);
+commentsRouter
+  .route("/:comment_id")
+  .patch(patchCommentById)
+  .delete(deleteCommentById)
+  .all(handle405);
 
 module.exports = commentsRouter;
