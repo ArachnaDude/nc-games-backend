d85a3377b977a61e0fa564d35d40f524de839fe0
Matt <cunning.distraction@googlemail.com>
2022-06-14 15:05:22 +0100

    add valid method to users router

--- routes/users.router.js
+++ routes/users.router.js
@@ -1,9 +1,13 @@
 const express = require("express");
-const { getUsers } = require("../controllers/getUsers.controllers");
+const { getUsers, getUserByUsername } = require("../controllers");
 const { handle405 } = require("../errors");
 
 const usersRouter = express.Router();
 
+// handles all methods associated with /api/users
 usersRouter.route("/").get(getUsers).all(handle405);
+
+// handles all methods associated with /api/users/username
+usersRouter.route("/:username").get(getUserByUsername).all(handle405);
 
 module.exports = usersRouter;
