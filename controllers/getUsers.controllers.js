9bd8d85f0788f07787295a1293c6ce70ae0919f8
Matt <cunning.distraction@googlemail.com>
2022-06-14 03:14:55 +0100

    create getUsers controller

--- controllers/getUsers.controllers.js
+++ controllers/getUsers.controllers.js
@@ -1,5 +1,9 @@
+const { selectUsers } = require("../models/selectUsers.models");
+
 exports.getUsers = async (req, res, next) => {
   try {
+    const users = await selectUsers();
+    res.status(200).send({ users });
   } catch (error) {
     next(error);
   }
