8b0e23055a7c3f90970adc97ac7b60adff9ac6a4
Matt <cunning.distraction@googlemail.com>
2022-06-14 03:16:35 +0100

    add valid route to user router

--- routes/users.router.js
+++ routes/users.router.js
@@ -1,8 +1,9 @@
 const express = require("express");
+const { getUsers } = require("../controllers/getUsers.controllers");
 const { handle405 } = require("../errors");
 
 const usersRouter = express.Router();
 
-usersRouter.route("/").get().all(handle405);
+usersRouter.route("/").get(getUsers).all(handle405);
 
 module.exports = usersRouter;
