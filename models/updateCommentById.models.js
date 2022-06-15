716bf15ec94deeb6c665269cf0b07b03e28e5328
Matt <cunning.distraction@googlemail.com>
2022-06-15 02:45:22 +0100

    refactor updateCommentById model to utilise async/await

--- models/updateCommentById.models.js
+++ models/updateCommentById.models.js
@@ -1,16 +1,13 @@
 const db = require("../db/connection");
 
-exports.updateCommentById = (comment_id, inc_votes = 0) => {
-  console.log(comment_id, inc_votes);
-  return db
-    .query(
-      `UPDATE comments SET votes = votes + $1 WHERE comment_id = $2 RETURNING *;`,
-      [inc_votes, comment_id]
-    )
-    .then((result) => {
-      if (!result.rowCount) {
-        return Promise.reject({ status: 404, message: "comment not found" });
-      }
-      return result.rows[0];
-    });
+exports.updateCommentById = async (comment_id, inc_votes = 0) => {
+  const comment = await db.query(
+    `UPDATE comments SET votes = votes + $1 WHERE comment_id = $2 RETURNING *;`,
+    [inc_votes, comment_id]
+  );
+
+  if (!comment.rowCount) {
+    return Promise.reject({ status: 404, message: "comment not found" });
+  }
+  return comment.rows[0];
 };
