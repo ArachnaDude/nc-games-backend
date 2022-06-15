a45529e14159a13134ac01fd2eecdedd9af05066
Matt <cunning.distraction@googlemail.com>
2022-06-15 02:33:39 +0100

    refactor deleteComment model to utilise async/await

--- models/deleteComment.models.js
+++ models/deleteComment.models.js
@@ -1,11 +1,11 @@
 const db = require("../db/connection");
 
-exports.deleteComment = (comment_id) => {
-  return db
-    .query(`DELETE FROM comments WHERE comment_id = $1;`, [comment_id])
-    .then((result) => {
-      if (!result.rowCount) {
-        return Promise.reject({ status: 404, message: "Not found" });
-      }
-    });
+exports.deleteComment = async (comment_id) => {
+  const deleteComment = await db.query(
+    `DELETE FROM comments WHERE comment_id = $1;`,
+    [comment_id]
+  );
+  if (!deleteComment.rowCount) {
+    return Promise.reject({ status: 404, message: "Not found" });
+  }
 };
