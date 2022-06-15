13ca2247e68e9510cbd996950939918619e1bca6
Matt <cunning.distraction@googlemail.com>
2022-06-15 02:30:37 +0100

    refactor insertCommentByReviewId model to utilise async/await

--- models/insertCommentByReviewId.models.js
+++ models/insertCommentByReviewId.models.js
@@ -1,12 +1,10 @@
 const db = require("../db/connection");
 
-exports.insertCommentByReviewId = (username, body, review_id) => {
-  return db
-    .query(
-      `INSERT INTO comments (author, body, review_id) VALUES ($1, $2, $3) RETURNING comment_id, votes, created_at, author, body;`,
-      [username, body, review_id]
-    )
-    .then((result) => {
-      return result.rows[0];
-    });
+exports.insertCommentByReviewId = async (username, body, review_id) => {
+  const comment = await db.query(
+    `INSERT INTO comments (author, body, review_id) VALUES ($1, $2, $3) RETURNING comment_id, votes, created_at, author, body;`,
+    [username, body, review_id]
+  );
+
+  return comment.rows[0];
 };
