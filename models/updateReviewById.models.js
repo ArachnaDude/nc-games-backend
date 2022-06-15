f5f32b38d66b57252218909670cccf7bcc9991fb
Matt <cunning.distraction@googlemail.com>
2022-06-15 02:51:04 +0100

    remove defunct comments

--- models/updateReviewById.models.js
+++ models/updateReviewById.models.js
@@ -1,16 +1,5 @@
 const db = require("../db/connection");
 
-// exports.updateReviewById = (body, params) => {
-//   return db
-//     .query(
-//       `UPDATE reviews SET votes = votes + $1 WHERE review_id = $2 RETURNING *;`,
-//       [body, params]
-//     )
-//     .then((result) => {
-//       return result.rows[0];
-//     });
-// };
-
 exports.updateReviewById = async (body, params) => {
   const review = await db.query(
     `UPDATE reviews SET votes = votes + $1 WHERE review_id = $2 RETURNING *;`,
