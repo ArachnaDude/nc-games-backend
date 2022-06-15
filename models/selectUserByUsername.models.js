f19965148b28a1166cfbc1df92af261c41f9d10f
Matt <cunning.distraction@googlemail.com>
2022-06-15 02:42:37 +0100

    refactor selectUserByUsername model to utilise async/await

--- models/selectUserByUsername.models.js
+++ models/selectUserByUsername.models.js
@@ -1,12 +1,11 @@
 const db = require("../db/connection");
 
-exports.selectUserByUsername = (username) => {
-  return db
-    .query(`SELECT * FROM users WHERE username = $1;`, [username])
-    .then((result) => {
-      if (!result.rowCount) {
-        return Promise.reject({ status: 404, message: "user not found" });
-      }
-      return result.rows[0];
-    });
+exports.selectUserByUsername = async (username) => {
+  const user = await db.query(`SELECT * FROM users WHERE username = $1;`, [
+    username,
+  ]);
+  if (!user.rowCount) {
+    return Promise.reject({ status: 404, message: "user not found" });
+  }
+  return user.rows[0];
 };
