40cd477d696fd7e2899ffdd3ca9784d244ad17e8
Matt <cunning.distraction@googlemail.com>
2022-06-15 02:36:07 +0100

    refactor selectUsers model to utilise async/await

--- models/selectUsers.models.js
+++ models/selectUsers.models.js
@@ -1,7 +1,6 @@
 const db = require("../db/connection");
 
-exports.selectUsers = () => {
-  return db.query(`SELECT username FROM users;`).then((result) => {
-    return result.rows;
-  });
+exports.selectUsers = async () => {
+  const users = await db.query(`SELECT username FROM users;`);
+  return users.rows;
 };
