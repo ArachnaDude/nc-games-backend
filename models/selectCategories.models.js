a102b28decde840f0b93db0831136f9beae45dac
Matt <cunning.distraction@googlemail.com>
2022-06-15 02:00:56 +0100

    refactor selectCategories to utilise async/await

--- models/selectCategories.models.js
+++ models/selectCategories.models.js
@@ -1,7 +1,12 @@
 const db = require("../db/connection");
 
-exports.selectCategories = () => {
-  return db.query(`SELECT * FROM categories;`).then((result) => {
-    return result.rows;
-  });
+// exports.selectCategories = () => {
+//   return db.query(`SELECT * FROM categories;`).then((result) => {
+//     return result.rows;
+//   });
+// };
+
+exports.selectCategories = async () => {
+  const categories = await db.query(`SELECT * FROM categories`);
+  return categories.rows;
 };
