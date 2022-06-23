3f0725ba39047adb4634c4e6375fdc74478e7931
Matt <cunning.distraction@googlemail.com>
2022-06-23 02:07:19 +0100

    remove console logs

--- errors/index.js
+++ errors/index.js
@@ -7,7 +7,6 @@ exports.handle405 = (req, res, next) => {
 };
 
 exports.handlePsqlErrors = (err, req, res, next) => {
-  console.log(err, "psql errors");
   const { code } = err;
   if (code === "22P02" || code === "23502" || code === "23505") {
     res.status(400).send({ message: "Bad request" });
@@ -20,7 +19,6 @@ exports.handlePsqlErrors = (err, req, res, next) => {
 };
 
 exports.handleCustomErrors = (err, req, res, next) => {
-  console.log(err, "custom errors");
   const { status, message } = err;
   res.status(status).send({ message });
 };
