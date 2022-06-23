15f7470850a3b525cd7044e8e9b7c028429270cc
Matt <cunning.distraction@googlemail.com>
2022-06-23 01:47:11 +0100

    change variable name to remove ambiguity

--- models/deleteComment.models.js
+++ models/deleteComment.models.js
@@ -1,11 +1,11 @@
 const db = require("../db/connection");
 
 exports.deleteComment = async (comment_id) => {
-  const deleteComment = await db.query(
+  const deletedComment = await db.query(
     `DELETE FROM comments WHERE comment_id = $1;`,
     [comment_id]
   );
-  if (!deleteComment.rowCount) {
+  if (!deletedComment.rowCount) {
     return Promise.reject({ status: 404, message: "Not found" });
   }
 };
