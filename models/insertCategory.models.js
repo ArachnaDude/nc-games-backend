ceeb7604cf03725e3822fc5b92dc25396203a95f
Matt <cunning.distraction@googlemail.com>
2022-06-23 01:24:57 +0100

    add default value for description

--- models/insertCategory.models.js
+++ models/insertCategory.models.js
@@ -1,9 +1,13 @@
 const db = require("../db/connection");
 
-exports.insertCategory = async (slug, description) => {
+exports.insertCategory = async (
+  slug,
+  description = "no description available"
+) => {
   const category = await db.query(
     `INSERT INTO categories (slug, description) VALUES ($1, $2) RETURNING *;`,
     [slug, description]
   );
+
   return category.rows[0];
 };
